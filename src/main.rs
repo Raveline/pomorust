960ebbcb0e82c55c226d54fee8ce582fe395bcd3
Raveline <eraveline@gmail.com>
2016-05-08 14:05:28 +0200

    Call to parse, import extern argparse

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
+#![feature(type_ascription)]
 extern crate uuid;
+extern crate argparse;
 
 pub mod pomorust;
 
 use pomorust::config::config::read_task_file;
+use pomorust::actions;
 
 fn main() {
     let task_file = read_task_file();
-    for t in task_file {
-        println!("Task : {}", t.to_string())
-    }
+    actions::parse();
 }
