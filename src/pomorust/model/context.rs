83687fa3473986e382eaab070484d5fea16daf42
Raveline <eraveline@gmail.com>
2016-05-20 14:36:58 +0200

    Method to check if a task is ongoing

--- src/pomorust/model/context.rs
+++ src/pomorust/model/context.rs
@@ -10,4 +10,9 @@ impl Context {
     pub fn add_task(&mut self, task: Task) {
         self.tasks.push(task);
     }
+
+    pub fn has_ongoing_task(&self) -> bool {
+        let ongoings = self.tasks.iter().find(|&x| x.is_ongoing);
+        ongoings.is_some()
+    }
 }
